00830904fb12c99f408a0a3c6945670fd842644d
Brian Miranda <brianmiranda.dev@gmail.com>
2025-01-22 18:34:21 -0500

    :poop: test color changes

--- lua/kanagawa-paper/highlights/syntax.lua
+++ lua/kanagawa-paper/highlights/syntax.lua
@@ -11,7 +11,7 @@ function M.setup(colors, opts)
 		Comment = vim.tbl_extend("force", { fg = theme.syn.comment }, opts.commentStyle),
 
 		-- *Constant	any constant
-		Constant = { fg = theme.syn.constant, bold = false },
+		Constant = { fg = theme.syn.constant },
 		--  String		a string constant: "this is a string"
 		String = { fg = theme.syn.string },
 		--  Character	a character constant: 'c', '\n'
